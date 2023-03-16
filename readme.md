48e451be86f5c2035b29f44c5f963ccc4d9e118c
Guadalupe <mendoza91guada@gmail.com>
2023-03-16 19:55:35 -0300

    Segundo commit, se completa ejercicio de puntero y se extiende contenido de .gitignore

---
+++ readme.md
@@ -0,0 +1,2 @@
+* Para indicarle a .gitignore que ignore un archivo de nombre 'ignorado.txt', basta con escribir el nombre dentro del contenido del archivo .gitignore. 
+* 
--- tp1_1.c
+++ tp1_1.c
@@ -3,6 +3,13 @@
 
 void main()
 {
-    printf("Hola mundo");
-    
+    int valor, *punt;
+    valor=10;
+    punt=&valor;
+    printf("Hola mundo\n");
+    printf("1- La direccion de memoria almacenada por el puntero es %p\n", punt);
+    printf("2- La direccion de memoria de la variable es %p\n", &valor);
+    printf("3- La direccion de memoria del puntero es %p\n", &punt);
+    printf("4- El tamano de memoria utilizada por el puntero es %d\n", sizeof(punt));
+    printf("5- El contenido del puntero es %d\n", *punt);
 }
